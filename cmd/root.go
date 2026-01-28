d9c68306a4ff0d277cdb30e7ba51c6ba1352f644
k1LoW <k1lowxb@gmail.com>
2026-01-29 00:07:22 +0900

    chore: remove unnecessary print

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/k1LoW/tcmux/output"
@@ -25,7 +24,6 @@ func init() {
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
